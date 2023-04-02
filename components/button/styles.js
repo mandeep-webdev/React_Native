b866be8bf7a72e4b8eefb2f0ecfe06fea5fdeb8e
Mandeep Kaur <mandeep.fullstack.dev@gmail.com>
2023-04-02 15:59:54 +0530

    done with button component

---
+++ components/button/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    padding: 20,
+  },
+  button: {
+    height: 40,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+  },
+  text: {
+    fontSize: 12,
+    fontWeight: '500',
+    textTransform: 'uppercase',
+  },
+});
+
+export default styles;
--- components/carItem/index.js
+++ components/carItem/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, ImageBackground } from 'react-native';
+import Button from '../button';
 import styles from './styles';
 
 const CartItem = () => {
@@ -13,6 +14,20 @@ const CartItem = () => {
         <Text style={styles.title}>Model S</Text>
         <Text style={styles.subtitle}>Starting at $70,000</Text>
       </View>
+      <Button
+        type="primary"
+        content={'Custom Order'}
+        onPress={() => {
+          console.warn('Custom Order is Pressed');
+        }}
+      />
+      <Button
+        type="secondary"
+        content={'Existing Inventory'}
+        onPress={() => {
+          console.warn('Existing Inventory is Pressed');
+        }}
+      />
     </View>
   );
 };
