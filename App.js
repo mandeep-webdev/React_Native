e6bfbacad88660a268c3a5109c5a330dcc55e6e3
Mandeep Kaur <mandeep.fullstack.dev@gmail.com>
2023-04-02 14:54:09 +0530

    first Commit

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, View } from 'react-native';
+import CartItem from './components/carItem/';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CartItem />
     </View>
   );
 }
